64e00bcb14353d2e4c035e48e2f2d6bc436aa95f
ZainTheCoder <zain5283@aia.qa>
2026-09-19 17:43:21 +0300

    Added logo and new name

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ Initial Commit Version - 1.1 with Customtkinter
 '''
 import customtkinter as ctk
 import MusicHandler
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(base_dir, "Assets")
 
 app = ctk.CTk()
 app.geometry("800x800")
-title = ctk.CTkLabel(app,text="Your collection").pack()
+app.title("Zainify")
+title = ctk.CTkLabel(app, text="Your collection").pack()
+
+icon_path = os.path.join(base_dir, "Assets", "Logo.ico")
+app.iconbitmap(icon_path)
 
 app.mainloop()
